2ac7ab375da24e3810bda38b8c0ff89b8d1ad622
Roman Ishchuk <i.roman.zp@gmail.com>
2026-08-13 22:32:53 +0200

    Add product and controller

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EcommerceApi.Models;
+
+namespace EcommerceApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetProducts()
+    {
+        var products = new[]
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 999.99m },
+            new Product { Id = 2, Name = "Mouse", Price = 29.99m }
+        };
+
+        return Ok(products);
+    }
+}
