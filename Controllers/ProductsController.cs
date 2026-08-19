0c29918dd269e7a1ebd618b16a491a44cc6cc438
Roman Ishchuk <i.roman.zp@gmail.com>
2026-08-19 21:30:21 +0200

    Add product CRUD with EF Core

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 ﻿using EcommerceApi.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using EcommerceApi.DTOs;
+using EcommerceApi.Models;
 
 namespace EcommerceApi.Controllers;
 
@@ -21,5 +23,71 @@ public class ProductsController : ControllerBase
         var products = await _context.Products.ToListAsync();
 
         return Ok(products);
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product is null)
+        {
+            return NotFound();
+        }
+        return Ok(product);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct(CreateProductDto dto)
+    {
+        var product = new Product
+        {
+            Name = dto.Name,
+            Price = dto.Price
+        };
+
+        _context.Products.Add(product);
+
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(
+            nameof(GetProduct),
+            new { id = product.Id },
+            product);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateProduct(int id, CreateProductDto dto)
+    {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        product.Name = dto.Name;
+        product.Price = dto.Price;
+
+        await _context.SaveChangesAsync();
+
+        return Ok(product);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        _context.Products.Remove(product);
+
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
---
+++ DTOs/CreateProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EcommerceApi.DTOs;
+
+public class CreateProductDto
+{
+    public string Name { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+}
